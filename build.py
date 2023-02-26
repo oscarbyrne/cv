475bd9ec7817664fcdb26f02f3069a36a13d0f3d
oscarbyrne <oscar.byrne@gmail.com>
2023-02-26 20:14:26 +0000

    Improve readability of specification.yaml

--- build.py
+++ build.py
@@ -16,27 +16,27 @@ def get_argument_parser():
     )
     defaults = yaml_from_path(CONFIG)
     parser.add_argument(
-        "-sf", "--specification-file",
+        '-sf', '--specification-file',
         help="YAML file specifying resume high-level properties and layout",
         default=defaults['specification_file']
     )
     parser.add_argument(
-        "-r", "--records-file",
+        '-r', '--records-file',
         help="YAML file containing records, used to populate resume",
         default=defaults['records_file']
     )
     parser.add_argument(
-        "-t", "--template-file",
+        '-t', '--template-file',
         help="J2 HTML template file for resume presentation logic",
         default=defaults['template_file']
     )
     parser.add_argument(
-        "-sd", "--static-dir",
+        '-sd', '--static-dir',
         help="Directory containing static resources (e.g. images and css)",
         default=defaults['static_dir']
     )
     parser.add_argument(
-        "-d", "--deploy-dir",
+        '-d', '--deploy-dir',
         help="Build directory for deployment",
         default=defaults['deploy_dir']
     )
@@ -50,18 +50,18 @@ def build_context(specification_file, records_file):
     context = yaml_from_path(specification_file)
     records = yaml_from_path(records_file)
     for page in context['pages']:
-        for section in page:
+        for section in page['sections']:
             section['records'] = [
-                records[name] for name in section["records"]
+                records[name] for name in section['records']
             ]
     return context
 
 def render_html(template_file, context):
     return jinja2.Environment(
-        loader=jinja2.FileSystemLoader("./"),
+        loader=jinja2.FileSystemLoader('./'),
         trim_blocks=True,
         lstrip_blocks=True,
-        line_statement_prefix="#"
+        line_statement_prefix='#'
     ).get_template(template_file).render(context)
 
 def build(html, static_dir, deploy_dir):
@@ -71,7 +71,7 @@ def build(html, static_dir, deploy_dir):
         dirs_exist_ok=True
     )
     output = os.path.join(deploy_dir, 'index.html')
-    with open(output, "w") as f:
+    with open(output, 'w') as f:
         f.write(html)
 
 def main(specification_file, records_file, template_file, static_dir, deploy_dir):
@@ -90,7 +90,7 @@ def main(specification_file, records_file, template_file, static_dir, deploy_dir
     )
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     opts = get_argument_parser().parse_args()
     main(
         **vars(opts)
