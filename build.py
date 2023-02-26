39c65b24374bf0d79dd17dca8b7635e9143a2e2e
oscarbyrne <oscar.byrne@gmail.com>
2023-02-26 08:22:52 +0000

    Seperate out style.css

--- build.py
+++ build.py
@@ -19,6 +19,11 @@ parser.add_argument(
     default="./resume/records.yaml"
 )
 parser.add_argument(
+    "-s", "--style_sheet",
+    help="css file",
+    default="./resume/style.css"
+)
+parser.add_argument(
     "-t", "--template",
     help="template Jinja2 HTML file",
     default="./resume/template.j2"
@@ -30,8 +35,11 @@ parser.add_argument(
 )
 
 
-def build_context(config, records):
+def build_context(config, records, style_sheet):
+    with open(config) as f: config = yaml.safe_load(f)
+    with open(records) as f: records = yaml.safe_load(f)
     context = copy.copy(config)
+    context["style_sheet"] = style_sheet
     for page in context["pages"]:
         for section in page:
             section["records"] = [
@@ -39,27 +47,19 @@ def build_context(config, records):
             ]
     return context
 
-def main(config, records, template, output):
-    with open(config) as f:
-        config = yaml.safe_load(f)
-    with open(records) as f:
-        records = yaml.safe_load(f)
-
-    context = build_context(config, records)
-
+def main(config, records, style_sheet, template, output):
+    context = build_context(config, records, style_sheet)
     html = jinja2.Environment(
         loader=jinja2.FileSystemLoader("./"),
         trim_blocks=True,
         lstrip_blocks=True,
         line_statement_prefix="#"
     ).get_template(template).render(context)
-
     with open(output, "w") as f:
         f.write(html)
 
 
 if __name__ == "__main__":
-
     opts = parser.parse_args()
     main(
         **vars(opts)
